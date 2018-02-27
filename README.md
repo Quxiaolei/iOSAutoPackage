cb4ad679dd25a404dc80a74559df4af35c7fdc69
xiaolei <xiaolei.qu@hotmail.com>
2018-02-27 14:20:31 +0800

    add file for autoPackage

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
 # iOSAutoPackage
+
+## 使用方法
+
+在工程根目录下运行下方命令即可:
+
+```python
+python autobuild.py -w XXX.xcworkspace -s XXX -m "1.测试description
+2.测试描述2
+3.测试描述3"
+# -w:表示workspace文件
+# -p:表示project文件
+# -s:表示打包对应的scheme
+# -m:表示打包更新描述信息,可不填
+```
+
+手动配置流程:
+
+1. 使用脚本时,需要首先指定打包类型`Release`或`Debug`
+2. 并在`exportOptions.plist`指定打包方法(常见有`app-store`,`ad-hoc`,`enterprise`,`package`,`development`)
+3. 指定ipa包输出文件夹(默认为`~/Desktop/AutoPackage/`)
+4. 设定蒲公英平台的`USER_KEY`和`API_KEY`
+5. 最后在工程文件夹下运行对应命令即可
+
+## 常见问题
+
+出现错误:
+
+```python
+import requests
+ImportError: No module named requests
+```
+
+使用`$ sudo pip install requests`或者`sudo easy_install -U requests`即可解决.
---
+++ autobuild.py
@@ -0,0 +1,305 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+# python autobuild.py -p youproject.xcodeproj -s schemename -m description
+# python autobuild.py -w youproject.xcworkspace -s schemename -m description
+
+import argparse
+import subprocess
+import requests
+import os, sys
+import time
+import commands
+
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from email.header import Header
+from email.mime.image import MIMEImage
+
+# configuration for iOS build setting
+# 会在桌面创建输出ipa文件的目录
+CONFIGURATION = "Release"
+# AutoPackage路径下
+EXPORT_OPTIONS_PLIST = "AutoPackage/exportOptions.plist"
+# 会在桌面创建输出ipa文件的目录
+EXPORT_MAIN_DIRECTORY = "~/Desktop/AutoPackage/"
+
+# configuration for pgyer
+PGYER_UPLOAD_URL = "http://www.pgyer.com/apiv1/app/upload"
+DOWNLOAD_BASE_URL = "http://www.pgyer.com"
+# 填写对应key值
+USER_KEY = ""
+API_KEY = ""
+# 设置从蒲公英下载应用时的密码
+PYGER_PASSWORD = ""
+# 蒲公英更新描述
+PGYDESC = "Default update description"
+# 应用内安装地址
+QuicklyInstallURL = ""
+
+# 邮件参数
+emailFromUser = ""
+# 多个邮箱使用,分割
+emailToUser = ["",""]
+acc = ["",""]
+emailPassword = ""
+emailHost = "smtp.exmail.qq.com"
+
+# 临时变量
+appBuildTime = ""
+
+def cleanArchiveFile(archiveFile):
+	cleanCmd = "rm -r %s" % (archiveFile)
+	process = subprocess.Popen(cleanCmd, shell=True)
+	process.wait()
+	print("cleaned archiveFile: %s" % (archiveFile))
+
+
+def parserUploadResult(jsonResult):
+	resultCode = jsonResult['code']
+	if resultCode == 0:
+		downUrl = DOWNLOAD_BASE_URL + "/" + jsonResult['data']['appShortcutUrl']
+		print("Upload Success")
+		print("DownUrl is:" + downUrl)
+		# 发送邮件
+		sendEmail(jsonResult)
+	else:
+		print("Upload Fail!")
+		print("Reason:" + jsonResult['message'])
+
+
+# 发邮件给测试不带附件
+def sendEmail(jsonResult):
+	downUrl = DOWNLOAD_BASE_URL +"/"+jsonResult['data']['appShortcutUrl']
+	appQRCodeURL = jsonResult['data']['appQRCodeURL']
+	appVersion = jsonResult['data']['appVersion']
+	appUpdateDescription = PGYDESC.replace("\n", "<br>")
+
+	print("\nappVersion:%s,\nappBuildTime:%s,\nappQRCodeURL:%s,\ndownUrl:%s\ndes:%s\nappUpdateDescription:%s\n---end" % (
+	appVersion, appBuildTime, appQRCodeURL, downUrl, PGYDESC, appUpdateDescription))
+
+	# 邮件正文第三方内容
+	msg = MIMEMultipart('related')
+	msgtext = MIMEText("<font><h3>iOS测试包_%s(%s)</h3><p><h4>更新说明：</h4>%s</font>	\
+    <p>安装二维码(最新包)：<br><img src='%s' /><br>具体版本信息详见：<a href='%s'>%s</a><p>应用内安装地址：<a href= '%s' >安装</a>(仅iOS 11以下机型可用)   \
+    <font color = gray><h5 >NOTICE：本邮件由系统自动发送，请勿回复。</h5></font>" % (
+	appVersion, appBuildTime, appUpdateDescription, appQRCodeURL, downUrl, downUrl, QuicklyInstallURL), "html", "utf-8")
+	msg.attach(msgtext)
+
+	msg['to'] = ",".join(emailToUser)
+	msg['cc'] = ",".join(acc)
+	msg['from'] = emailFromUser
+	msg['subject'] = '[iOS测试]新的iOS测试包已经上传，请注意查收'
+
+	try:
+		server = smtplib.SMTP()
+		server.connect(emailHost)
+		server.login(emailFromUser, emailPassword)
+		server.sendmail(msg['from'], emailToUser+acc, msg.as_string())
+		server.quit()
+		print('发送成功')
+	except Exception, e:
+		print(str(e))
+	return
+
+
+def uploadIpaToPgyer(ipaPath):
+	print("uploadIpaToPgyer_ipaPath:" + ipaPath)
+	if USER_KEY == "" or API_KEY == "":
+		return
+	ipaPath = os.path.expanduser(ipaPath)
+	ipaPath = unicode(ipaPath, "utf-8")
+	files = {'file': open(ipaPath, 'rb')}
+	headers = {'enctype': 'multipart/form-data'}
+	payload = {'uKey': USER_KEY, '_api_key': API_KEY, 'publishRange': '2', 'isPublishToPublic': '2',
+	           'password': PYGER_PASSWORD, 'updateDescription': PGYDESC}
+	print("update desc：%s" % (PGYDESC))
+	print("uploading....")
+	r = requests.post(PGYER_UPLOAD_URL, data=payload, files=files, headers=headers)
+	if r.status_code == requests.codes.ok:
+		result = r.json()
+		parserUploadResult(result)
+	else:
+		print('HTTPError,Code:' + r.status_code)
+
+
+# 创建输出ipa文件路径: ~/Desktop/AutoPackage/{scheme}{2018-2-27_14-05-10}
+def buildExportDirectory(scheme):
+	dateCmd = 'date "+%Y-%m-%d_%H-%M-%S"'
+	process = subprocess.Popen(dateCmd, stdout=subprocess.PIPE, shell=True)
+	(stdoutdata, stderrdata) = process.communicate()
+	exportDirectory = "%s%s%s" % (EXPORT_MAIN_DIRECTORY, scheme, stdoutdata.strip())
+	return exportDirectory
+
+
+def buildArchivePath(tempName):
+	process = subprocess.Popen("pwd", stdout=subprocess.PIPE)
+	(stdoutdata, stderrdata) = process.communicate()
+	archiveName = "%s.xcarchive" % (tempName)
+	archivePath = "%s/%s" % (stdoutdata.strip(), archiveName)
+	return archivePath
+
+
+def getNewIpaPath(exportPath, scheme):
+	ipaName = scheme + ".ipa"
+	ipaPath = exportPath + "/" + ipaName
+	print("getNewIpaPath_ipaPath: " + ipaPath)
+	return ipaPath
+
+
+def getIpaPath(exportPath):
+	cmd = "ls %s" % (exportPath)
+	process = subprocess.Popen(cmd, stdout=subprocess.PIPE, shell=True)
+	(stdoutdata, stderrdata) = process.communicate()
+	ipaName = stdoutdata.strip()
+	ipaPath = exportPath + "/" + ipaName
+	print("getIpaPath_ipaPath: " + ipaPath)
+	return ipaPath
+
+
+def exportArchive(scheme, archivePath):
+	exportDirectory = buildExportDirectory(scheme)
+	exportCmd = "xcodebuild -exportArchive -archivePath %s -exportPath %s -exportOptionsPlist %s" % (
+	archivePath, exportDirectory, EXPORT_OPTIONS_PLIST)
+	process = subprocess.Popen(exportCmd, shell=True)
+	(stdoutdata, stderrdata) = process.communicate()
+
+	signReturnCode = process.returncode
+	if signReturnCode != 0:
+		print("export %s failed" % (scheme))
+		return ""
+	else:
+		return exportDirectory
+
+
+def changeBundle_identifier(workspace, scheme):
+	# 获取项目名称
+	project_name = workspace.split(".")[0]
+	# 获取版本号,内部版本号,bundleID
+	info_plist_path = "%s/%s.plist" % (project_name, scheme)
+	# 3.1.0
+	bundle_version = commands.getstatusoutput("/usr/libexec/PlistBuddy -c 'Print CFBundleShortVersionString' %s" % (info_plist_path))[1]
+	# $(PRODUCT_BUNDLE_IDENTIFIER)
+	bundle_build_version = commands.getstatusoutput("/usr/libexec/PlistBuddy -c 'Print CFBundleIdentifier' %s" % (info_plist_path))[1]
+	# 0226_142435
+	bundle_identifier = commands.getstatusoutput("/usr/libexec/PlistBuddy -c 'Print CFBundleVersion' %s" % (info_plist_path))[1]
+
+	# 更新build号
+	timestamp = time.strftime("%m%d_%H%M%S")
+	commands.getstatusoutput("/usr/libexec/PlistBuddy -c 'Set :CFBundleVersion %s' '%s'" % (timestamp, info_plist_path))
+
+	global appBuildTime
+	appBuildTime = timestamp
+	print("project_name: %s,\nbundle_version:%s,\nbundle_build_version:%s,\nbundle_identifier:%s" % (
+	project_name, bundle_version, bundle_build_version, bundle_identifier))
+
+
+def buildProject(project, scheme):
+	# 更新build号
+	changeBundle_identifier(project, scheme)
+
+	# clean 工程
+	cleanCmd = "xcodebuild clean -workspace %s -scheme %s -configuration %s -alltargets" % (
+	workspace, scheme, CONFIGURATION)
+	process = subprocess.Popen(cleanCmd, shell=True)
+	process.wait()
+	print("\n\033[32m************************* clean完成 ************************* \033[0m\n")
+
+	archivePath = buildArchivePath(scheme)
+	print("archivePath: " + archivePath)
+	archiveCmd = 'xcodebuild -project %s -scheme %s -configuration %s archive -archivePath %s -destination generic/platform=iOS' % (
+	project, scheme, CONFIGURATION, archivePath)
+	process = subprocess.Popen(archiveCmd, shell=True)
+	process.wait()
+
+	archiveReturnCode = process.returncode
+	if archiveReturnCode != 0:
+		print("archive workspace %s failed" % (workspace))
+		cleanArchiveFile(archivePath)
+	else:
+		exportDirectory = exportArchive(scheme, archivePath)
+		cleanArchiveFile(archivePath)
+		if exportDirectory != "":
+			ipaPath = getIpaPath(exportDirectory)
+			uploadIpaToPgyer(ipaPath)
+
+
+def buildWorkspace(workspace, scheme):
+	# 更新build号
+	changeBundle_identifier(workspace, scheme)
+
+	# 更新pod 仓库
+	print("\n\033[32m************************* 开始pod ************************* \033[0m")
+	podCmd = 'pod install --verbose --no-repo-update'
+	process = subprocess.Popen(podCmd, shell=True)
+	process.wait()
+	print("\033[32m************************* pod完成 ************************* \033[0m\n")
+
+	# clean 工程
+	cleanCmd = "xcodebuild clean -workspace %s -scheme %s -configuration %s -alltargets" % (
+	workspace, scheme, CONFIGURATION)
+	process = subprocess.Popen(cleanCmd, shell=True)
+	process.wait()
+	print("\n\033[32m************************* clean完成 ************************* \033[0m\n")
+
+	archivePath = buildArchivePath(scheme)
+	print("buildWorkspace_archivePath: " + archivePath)
+	archiveCmd = 'xcodebuild -workspace %s -scheme %s -configuration %s archive -archivePath %s -destination generic/platform=iOS' % (
+	workspace, scheme, CONFIGURATION, archivePath)
+	process = subprocess.Popen(archiveCmd, shell=True)
+	process.wait()
+
+	archiveReturnCode = process.returncode
+	if archiveReturnCode != 0:
+		print("archive workspace %s failed" % (workspace))
+		cleanArchiveFile(archivePath)
+	else:
+		exportDirectory = exportArchive(scheme, archivePath)
+		cleanArchiveFile(archivePath)
+		if exportDirectory != "":
+			print("buildWorkspace_exportDirectory: " + exportDirectory)
+			# 旧的getIpaPath方法会出现莫名其妙的bug
+			ipaPath = getNewIpaPath(exportDirectory, scheme)
+			uploadIpaToPgyer(ipaPath)
+
+
+def xcbuild(options):
+	project = options.project
+	workspace = options.workspace
+	scheme = options.scheme
+	desc = options.desc
+
+	global PGYDESC
+	if desc != "":
+		PGYDESC = desc
+
+	if project is None and workspace is None:
+		pass
+	elif project is not None:
+		buildProject(project, scheme)
+	elif workspace is not None:
+		buildWorkspace(workspace, scheme)
+
+
+def main():
+	# 转换编码格式
+	reload(sys)
+	sys.setdefaultencoding('utf8')
+
+	parser = argparse.ArgumentParser()
+	parser.add_argument("-w", "--workspace", help="Build the workspace name.xcworkspace.", metavar="name.xcworkspace")
+	parser.add_argument("-p", "--project", help="Build the project name.xcodeproj.", metavar="name.xcodeproj")
+	parser.add_argument("-s", "--scheme",
+	                    help="Build the scheme specified by schemename. Required if building a workspace.",
+	                    metavar="schemename")
+	parser.add_argument("-m", "--desc", help="Pgyer update description.", metavar="description")
+	options = parser.parse_args()
+
+	print("options: %s" % (options))
+
+	xcbuild(options)
+
+
+if __name__ == '__main__':
+	main()
